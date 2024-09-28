d194dcc740e1e2b970dc3f86809322857a77fbda
RafikBELAZOUZ <belazouzrafik@outlook.com>
2024-09-28 16:56:53 +0100

    add google analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import Footer from '@/components/Footer';
 import ClientProviders from '@/components/Providers';
 import Navigation from '@/components/Navigation';
 import Fathom from '@/components/Fathom';
+import { GoogleAnalytics } from '@next/third-parties/google'
 
 import { NtDapperFont } from '@/fonts';
 import { isProduction } from '@/utils';
@@ -28,6 +29,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         </ClientProviders>
         {isProduction() && <Fathom />}
       </body>
+      <GoogleAnalytics gaId="G-Q8L6LF2BFV" />
     </html>
   );
 }
