a75be5b074b29b6961082ca51402d68c65a4145a
RafikBELAZOUZ <belazouzrafik@outlook.com>
2024-09-28 18:37:01 +0100

    set diagram page

---
+++ src/app/dashboard/testdiagram/page.tsx
@@ -0,0 +1,11 @@
+import SubscriptionFeature from '@/components/SubscriptionFeature';
+
+function DiagramPage() {
+  return (
+    <>
+      <div>Im home Diagram page</div>
+    </>
+  );
+}
+
+export default DiagramPage;
